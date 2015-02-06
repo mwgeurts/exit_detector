285d929e71ff43f0aaab0a4aea4e4d82fff9c263
Mark Geurts <mark.w.geurts@gmail.com>
2015-02-06 15:02:18 -0600

    Changed calcRefDose to 1

--- ExitDetector.m
+++ ExitDetector.m
@@ -213,7 +213,7 @@ Event(sprintf('Hide fluence delivery plan flag set to %i', ...
 % if the beam model differs significantly from the actual TPS, as dose
 % difference/gamma comparison will now compare two dose distributions
 % computed using the same model
-handles.calcRefDose = 0;
+handles.calcRefDose = 1;
 Event(sprintf('Recalculate reference dose flag set to %i', ...
     handles.calcRefDose));
 
