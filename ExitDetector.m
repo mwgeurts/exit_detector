a215789239d5913b8964c68f45b0c12b8849e0fe
Mark Geurts <mark.w.geurts@gmail.com>
2017-04-27 08:24:34 -0500

    Backwards compatibility fix

--- ExitDetector.m
+++ ExitDetector.m
@@ -66,7 +66,7 @@ warning('off','all');
 handles.output = hObject;
 
 % Set version handle
-handles.version = '1.3.0';
+handles.version = '1.3.1';
 
 % Determine path of current application
 [path, ~, ~] = fileparts(mfilename('fullpath'));
